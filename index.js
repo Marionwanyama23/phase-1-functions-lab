9c2ff9bbace3169a02c63a155521aa6eace84fd4
+ <+>
2022-06-11 20:03:01 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (blocknNumber) {
+    if (blocknNumber > 42){
+        return blocknNumber - 42
+    }
+    else {
+        return 42 - blocknNumber
+    }
+}
+function distanceFromHqInFeet (blocknNumber){
+    return distanceFromHqInBlocks (blocknNumber) * 264
+}
+function distanceTravelledInFeet (start, destination) {
+    if (start < destination) {
+        return (destination - start) * 264
+    }
+}
+function calculatesFarePrice (start, destination) {
+    const feet = distanceTravelledInFeet ( start, destination)
+    if (feet <= 400) {
+        return 0
+    } 
+    else if (feet > 400 && feet < 2000){
+        return 0.02 * (feet - 400)
+    }
+    else if (feet > 2000 && feet <2500){
+        return 25
+    }
+    else {
+        return "cannot travel that far"
+    }
+}
